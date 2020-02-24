fbdaaac8c0cda02102c914ee00ff2451268d8143
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-24 02:54:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,11 +33,7 @@ self.all.find {|s| s.name == name}
 end
 
 def self.find_or_create_by_name(name)
-if self.find_by_name(name) == false
-  self.create_by_name(name)
-else
-  self.find_by_name(name)
-end
+
 end
 
 def self.alphabetical
