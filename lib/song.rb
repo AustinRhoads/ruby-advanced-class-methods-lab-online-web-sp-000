1323ea32eb6aa7988e5d17f061058178fb9c02d8
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-24 02:06:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,46 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+song = Song.new
+song.save
+return song
+  end
+
+def self.new_by_name(name)
+song = Song.new
+song.name = name
+song
+end
+
+def self.create_by_name(name)
+  song = Song.create
+  song.name = name
+  song
+end
+
+def self.find_by_name(name)
+self.class.all.find(name == name)
+end
+
+def find_or_creat_by_name
+
+end
+
+def self.alphabetical
+all.name.sort
+end
+
+def new_from_filename
+
+end
+
+def create_from_filename
+
+end
+
+def self.destroy_all
+self.all.clear
+end
+
 end
